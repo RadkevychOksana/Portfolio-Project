09f54e75e0f67cf853ebcb681224cd6fb54c1a93
Radkevych Oksana <radkevych.oksana@gmail.com>
2022-08-12 21:36:25 +0200

    added multilingual script

---
+++ js /lang.js
@@ -0,0 +1,17 @@
+const langArr = {
+  about: {
+    ua: "Привіт! Я розробник інтерфейсу, і ось веб-сайт мого портфоліо. Тут ви дізнаєтеся про мій шлях як розробника інтерфейсу.",
+    en: "Hello! Am a frontend developer and here is my portfolio website. Here you’ll learn about my journey as a frontend developer.",
+    de: "Hallo! Bin Frontend-Entwickler und hier ist meine Portfolio-Website. Hier erfahren Sie mehr über meinen Werdegang als Frontend-Entwickler.",
+  },
+  unit: {
+    ua: "Головна",
+    en: "Home",
+    de: "Hauptseite",
+  },
+  name: {
+    ua: "Оксана Радкевич",
+    en: "Oksana Radkevych",
+    de: "Oksana Radkewitsch",
+  },
+};
