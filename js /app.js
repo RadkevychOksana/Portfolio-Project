9e167eaf0f80230d4ca73f03e7399ca1d3cf5b47
Radkevych Oksana <radkevych.oksana@gmail.com>
2022-08-13 13:53:06 +0200

    Updated content in 3 languages

--- js /app.js
+++ js /app.js
@@ -4,18 +4,19 @@ select.addEventListener("change", changeUrlLanguage);
 
 function changeUrlLanguage() {
   let lang = select.value;
-  location.href = window.location.pathname + "#" + lang;
-  location.reload;
+  window.location.href = window.location.pathname + "#" + lang;
+  changeLanguage();
 }
 
 function changeLanguage() {
   let hash = window.location.hash;
   hash = hash.substr(1);
   if (!allLang.includes(hash)) {
-    location.href = window.location.pathname + "#en";
-    location.reload;
+    window.location.href = window.location.pathname + "#en";
+    window.location.reload();
   }
   select.value = hash;
+  document.querySelector('title').innerHTML = langArr['unit'][hash];
   for (let key in langArr) {
     let elem = document.querySelector(".lng-" + key);
     if (elem) {
--- js /lang.js
+++ js /lang.js
@@ -1,8 +1,8 @@
 const langArr = {
   about: {
-    ua: "Привіт! Я розробник інтерфейсу, і ось веб-сайт мого портфоліо. Тут ви дізнаєтеся про мій шлях як розробника інтерфейсу.",
-    en: "Hello! Am a frontend developer and here is my portfolio website. Here you’ll learn about my journey as a frontend developer.",
-    de: "Hallo! Bin Frontend-Entwickler und hier ist meine Portfolio-Website. Hier erfahren Sie mehr über meinen Werdegang als Frontend-Entwickler.",
+    ua: "Привіт! Я Фронтенд - розробник, і ось веб сайт мого портфоліо. Тут ви дізнаєтеся про мій шлях як розробника інтерфейсів.",
+    en: "Hi! I'm a frontend - developer and here is my portfolio website. Here you can learn more about my adventures as a frontend developer.",
+    de: "Hallo! Ich bin Frontend - Entwickler und hier ist meine Portfolio Website. Hier erfahren Sie mehr über meine Abenteuer als Frontend - Entwickler.",
   },
   unit: {
     ua: "Головна",
@@ -12,6 +12,71 @@ const langArr = {
   name: {
     ua: "Оксана Радкевич",
     en: "Oksana Radkevych",
-    de: "Oksana Radkewitsch",
+    de: "Oksana Radkevych",
+  },
+  skills: {
+    ua: "Навички",
+    en: "Skills",
+    de: "Fähigkeiten",
+  },
+  skillsDescrition: {
+    ua: "Маю досвід роботи з такими веб-технологіями:",
+    en: "I have experience working with the following web technologies:",
+    de: "Ich habe Erfahrung in der Arbeit mit den folgenden Webtechnologien:",
+  },
+  projects: {
+    ua: "Проекти",
+    en: "Projects",
+    de: "Projekte",
+  },
+  projectsDescription: {
+    ua: "Деякі з моїх проектів включають:",
+    en: "Some of my projects include:",
+    de: "Einige meiner Projekte umfassen:",
+  },
+  weatherAppDescription: {
+    ua: "Веб-додаток прогнозу погоди, написаний на Java Sript фреймворці React, з використанням",
+    en: "Weather forecast web-application coded in the react-framework, using",
+    de: "Wettervorhersage-Webanwendung, die im React-Framework codiert ist und verwendet",
+  },
+  dictionaryAppDescription: {
+    ua: "Веб-додаток Dictionary, написаний на Java Sript фреймворці React, з використанням",
+    en: "Dictionary web-application coded in the the react-framework, using",
+    de: "Dictionary-Webanwendung, die im React-Framework codiert ist, mit",
+  },
+  socialLinks: {
+    ua: "Знайти мене в соціальних мережах або написати мені листа:",
+    en: "My social media links:",
+    de: "Meine Social-Media-Links:",
+  },
+  githubLinks: {
+    ua: "Ознайомитися з моїм кодом",
+    en: "Where to get my code",
+    de: "Wo bekommt man meinen Code:",
+  },
+  onGithub: {
+    ua: "на",
+    en: "on",
+    de: "auf",
+  },
+  onCodeWars: {
+    ua: "на",
+    en: "on",
+    de: "auf",
+  },
+  skillsLink: {
+    ua: "Навички",
+    en: "Skills",
+    de: "Fähigkeiten",
+  },
+  projectsLink: {
+    ua: "Проекти",
+    en: "Projects",
+    de: "Projekte",
+  },
+  contactsLink: {
+    ua: "Контакти",
+    en: "Contacts",
+    de: "Kontakte",
   },
 };
